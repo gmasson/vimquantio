c9aaa6c29663fab490b8eac61f692018d6ae4b4d
Gabriel Masson <gmasson@users.noreply.github.com>
2016-04-25 11:02:24 -0300

    Create install.py

---
+++ install.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+database = conn.cursor()
+
+# Criando a tabela (Dados Minerados)
+database.execute("""
+CREATE TABLE mining (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        url TEXT NOT NULL,
+        title TEXT NOT NULL,
+        text TEXT NOT NULL,
+        date_verification TEXT NOT NULL
+);
+""")
+
+conn.commit()
+
+# Criando a tabela (Cache)
+database.execute("""
+CREATE TABLE explorer_urls (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        url TEXT NOT NULL,
+        html TEXT NOT NULL
+);
+""")
+
+conn.commit()
+
+print('Tabelas criadas com sucesso.')
+
+# Desconectando...
+conn.close()
