1db069293e040ab708ad79cd48aba2971c1d6b97
Gabriel Masson <gmasson@users.noreply.github.com>
2016-05-26 13:56:40 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ------------------- 
+# Arquivo de configuração
+# ------------------- 
+
+# URL para ser minerada
+website = "http://gmasson.com.br/"
+
+# TAG á ser minerada
+tag_open = "<title"
+tag_close = "</title>"
